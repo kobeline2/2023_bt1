d1609834621563deab69f2ba1637916aed93a3a1
Takahiro Koshiba <kobeline2@gmail.com>
2024-01-07 21:47:04 +0200

    reserve par()

--- analysis/easy_regression.R
+++ analysis/easy_regression.R
@@ -3,13 +3,11 @@
 # data -----------
 d <- select(all_data, 'mass', 'duration', 'result')
 d <- na.omit(d)
-d$x <- d$result
-d$y <- d$mass / d$duration
+d$x <- log(d$result)
+d$y <- log(d$mass / d$duration)
 
 # linear regression ----
 # plot(y~x, data=d, log = "xy")
-# reg <- lm(log(y)~log(x), data=d)
-par(mfrow = c(1, 1))
 plot(y~x, data=d)
 reg <- lm(y~x, data=d)
 abline(reg, untf = TRUE)
@@ -17,7 +15,7 @@ plot_residual(reg, d$x)
 
 # exponential regression ----
 result <- nls(y ~ a*exp(b*x),
-           start=list(a=10, b=0.001),
+           start=list(a=1, b=0.001),
            data=d,
            trace=TRUE)
 summary(result)
--- utils/funcs.R
+++ utils/funcs.R
@@ -1,9 +1,11 @@
 plot_residual <- function(reg, x){
+  old_par <- par()
   par(mfrow = c(2, 1))
   res <- residuals(reg)
   plot(x, res, type='p')
   abline(h=0, lty=2)
   acf(residuals(reg), lag.max=10)
+  par(old_par)
 }
 
 plot_result_nls <- function(result, d){
@@ -14,9 +16,11 @@ plot_result_nls <- function(result, d){
   lines(dnew$x, pre)
   
   # plot residuals
+  old_par <- par()
   par(mfrow = c(2, 1))
   res <- residuals(result)
   plot(d$x, res, type='p')
   abline(h=0, lty=2)
   acf(residuals(result), lag.max=10)
+  par(old_par)
 }
