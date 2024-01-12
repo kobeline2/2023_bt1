d7f4055a1059aa9eabd103c12108d5e9a1c150e5
yukiakatsuka <yuki.471.aaa@gmail.com>
2024-01-12 19:12:07 +0900

    appdate

--- run.R
+++ run.R
@@ -1,6 +1,6 @@
 source('.Rprofile')
 source('main.R')
 # all_dataをRDSファイルとして保存
-saveRDS(all_data, file = "res/all_data.rds")
+saveRDS(all_data, file = "res/all_data_GARCH1.rds")
 # RDSファイルを読み込み
-all_data <- readRDS(file = "res/all_data.rds")
+all_data <- readRDS(file = "res/all_data_max.rds")
