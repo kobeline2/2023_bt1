8fd43668185a7a75ffe88d59a1918d8b0d98c0a0
yukiakatsuka <yuki.471.aaa@gmail.com>
2024-01-15 17:08:48 +0900

    'appdate'

---
+++ model/GARCH-ARMA1.R
@@ -0,0 +1,14 @@
+do_process <- function(data, metadata){
+  # ARMA-GARCHモデルの設定
+  spec <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1, 1)),
+                     mean.model = list(armaOrder = c(1, 1), include.mean = TRUE),
+                     distribution.model = "norm")#distribution.modelは使用する確率分布
+  #ged-一般化エラー分布が一番volatiが大きい
+  
+  # モデルの適合
+  fit <- ugarchfit(spec, data = data, solver = 'hybrid')
+  # ボラティリティの推定結果の取得
+  volatility <- as.numeric(sigma(fit))
+  z = sum(volatility)
+  return(z)
+}
--- run.R
+++ run.R
@@ -1,6 +1,9 @@
 source('.Rprofile')
 source('main.R')
 # all_dataをRDSファイルとして保存
-saveRDS(all_data, file = "res/all_data_GARCH1.rds")
+saveRDS(all_data, file = "res/all_data_GARCH-ARMA1.rds")
 # RDSファイルを読み込み
-all_data <- readRDS(file = "res/all_data_GARCH1.rds")
+all_data <- readRDS(file = "res/all_data_GARCH-ARMA1.rds")
+
+#all_data_GARCH1
+#all_data_GARCH-ARMA1
