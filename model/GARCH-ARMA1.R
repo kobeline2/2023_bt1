5f68ed5fb3146a36d81bf05a954e11a2ab5ab8a6
yukiakatsuka <yuki.471.aaa@gmail.com>
2024-01-24 16:09:27 +0900

    update

--- model/GARCH-ARMA1.R
+++ model/GARCH-ARMA1.R
@@ -2,7 +2,7 @@ do_process <- function(data, metadata){
   # ARMA-GARCHモデルの設定
   library(rugarch)
   spec <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1, 1)),
-                     mean.model = list(armaOrder = c(1, 1), include.mean = TRUE),
+                     mean.model = list(armaOrder = c(3, 1), include.mean = TRUE),
                      distribution.model = "norm")#distribution.modelは使用する確率分布
   #ged-一般化エラー分布が一番volatiが大きい
   
--- run.R
+++ run.R
@@ -1,9 +1,9 @@
 source('.Rprofile')
 source('main.R')
 # all_dataをRDSファイルとして保存
-saveRDS(all_data, file = "res/all_data_GARCH-ARMA1.rds")
+saveRDS(all_data, file = "res/all_data_GARCH(3,2)1.rds")
 # RDSファイルを読み込み
-all_data <- readRDS(file = "res/all_data_GARCH-ARMA1.rds")
+all_data <- readRDS(file = "res/all_data_GARCH(3,1)1.rds")
 
 #all_data_GARCH1
 #all_data_GARCH-ARMA1
