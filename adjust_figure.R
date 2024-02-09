88fad8694c2b976885851240e66dc2ffc7153201
kobeline2 <kobeline2@gmail.com>
2024-02-09 11:47:26 +0900

    figure updated

--- adjust_figure.R
+++ adjust_figure.R
@@ -2,7 +2,29 @@
 library(ggplot2)
 source('param.R')
 source(MODEL_PATH)
+
 font = "Times New Roman"
+fsz = 9
+fn = "plot.png"
+w = 8
+h = 5
+
+custom_theme <- function(font, fsz, w, h, fn) {
+  theme_classic()+
+  theme_minimal(base_family = font, base_size = fsz) +  # 基本のテキストサイズを9ptに設定
+  theme(
+    plot.margin = margin(t = 0.2, r = 0.2, b = 0.2, l = 0.2, "cm"),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    plot.title = element_text(size = fsz, color = "black", family = font),
+    axis.title.x = element_text(hjust = 0.5, vjust = 0,  size = fsz, color = "black", family = font),
+    axis.title.y = element_text(hjust = 1.0, vjust = 0, size = fsz, color = "black", family = font),
+    axis.text.x = element_text(hjust = 0.5, vjust = 0.0, size = fsz, color = "black", family = font),
+    axis.text.y = element_text(hjust = 0.5, vjust = 0.5, size = fsz, color = "black", family = font),
+    panel.border = element_rect(color = "black", fill = NA, linewidth = 1),
+    axis.ticks = element_line(color = "black")
+  )
+}
 
 #####
 data_id = 100
@@ -18,23 +40,9 @@ res <- do_process(d, all_data[i, ])
 d <- data.frame(x = seq_along(d), y = d)
 p <- ggplot(d, aes(x = x, y = y)) +
   geom_line() +
-  theme_classic()+
-  theme_minimal(base_family = font, base_size = 9) +  # 基本のテキストサイズを9ptに設定
-  theme(
-    plot.margin = margin(t = 0.2, r = 0.2, b = 0.2, l = 0.2, "cm"),
-    panel.grid.major = element_blank(),  # 主要なグリッド線を消す
-    panel.grid.minor = element_blank(),  # 補助的なグリッド線も消す
-    plot.title = element_text(size = 9, color = "black", family = font),
-    axis.title.x = element_text(hjust = 0.5, size = 9, color = "black", family = font),
-    axis.title.y = element_text(hjust = 1, vjust = 0, size = 9, color = "black", family = font),
-    axis.text.x = element_text(hjust = 0.5, size = 9, color = "black", family = font),
-    axis.text.y = element_text(vjust = 0.5, size = 9, color = "black", family = font),
-    panel.border = element_rect(color = "black", fill = NA, linewidth = 1)
-  )+ 
-  theme(axis.ticks = element_line(color = "black"))+
+  custom_theme(font, fsz, w, h, fn) +
   scale_x_continuous(breaks = c(0, 50000, 100000, 150000))+
   # scale_x_continuous(breaks = c(1, 2, 3, 4, 5), labels = c("A", "B", "C", "D", "E"))+
   geom_hline(yintercept = res, linetype = "dashed", color = "red") +
   labs(x = "Sample Points [-]", y = "Signal Amplitude [mV]")
-
-# ggsave("plot.png", plot = p, width = 8, height = 4, units = "cm", dpi = 600)
+ggsave(fn, plot = p, width = w, height = h, units = "cm", dpi = 600)
